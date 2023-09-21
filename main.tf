d8ee12b7adce1107560470c9a94336a15d16f572
Onovae <maureenonovae@gmail.com>
2023-09-21 13:24:17 +0100

    my first terraform project

--- main.tf
+++ main.tf
@@ -1,22 +1,22 @@
-#provider "aws" {
-# region = "us-east-1"
-#}
+
+
 provider "aws" {
-  region     = "us-east-1"
-  access_key = "AKIA6EGWBZSSO6XA3VXJ"
-  secret_key = "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"
+  region  = "us-east-1"
+  profile = "maureenc5"
 }
 
+
+# create a Linux instance
 #resource "aws_instance" "maureen-tf-server" {
 # instance_type = "t2.micro"
 #  tags = {
- #   Name = "Maureen-tf_Project_01"
- #   }
- # }
+#   Name = "Maureen-tf_Project_01"
+#   }
+# }
 
 #Create a vpc
 resource "aws_vpc" "maureen-tf-vpc" {
-  cidr_block       = "10.0.0.0/16"
+  cidr_block = "10.0.0.0/16"
   # instance_tenancy = "default"
 
   tags = {
@@ -27,7 +27,7 @@ resource "aws_vpc" "maureen-tf-vpc" {
 # Create an internet gateway and reference to the vpc created
 resource "aws_internet_gateway" "Maureen_prod_Igw" {
   vpc_id = aws_vpc.maureen-tf-vpc.id
-   tags = {
+  tags = {
     Name = "Maureen_Igw"
   }
 
@@ -42,7 +42,7 @@ resource "aws_route_table" "Maureen_Prod_route_table" {
     gateway_id = aws_internet_gateway.Maureen_prod_Igw.id
   }
 
- # route {
+  # route {
   #  ipv6_cidr_block        = "::/0"
   #  gateway_id = aws_internet_gateway.Maureen_prod_Igw.id
   #}
@@ -54,8 +54,8 @@ resource "aws_route_table" "Maureen_Prod_route_table" {
 
 #Create a subnet in an avalability zone
 resource "aws_subnet" "subnet-1" {
-  vpc_id     = aws_vpc.maureen-tf-vpc.id
-  cidr_block = "10.0.1.0/24"
+  vpc_id            = aws_vpc.maureen-tf-vpc.id
+  cidr_block        = "10.0.1.0/24"
   availability_zone = "us-east-1a"
 
   tags = {
@@ -76,35 +76,35 @@ resource "aws_security_group" "allow_web" {
   vpc_id      = aws_vpc.maureen-tf-vpc.id
 
   ingress {
-    description      = "HTTPS"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
     #ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
   }
   ingress {
-    description      = "HTTP"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
     #ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
   }
   ingress {
-    description      = "SSH"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
     #ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
     #ipv6_cidr_blocks = ["::/0"]
   }
 
@@ -126,33 +126,33 @@ resource "aws_eip" "one" {
   domain                    = "vpc"
   network_interface         = aws_network_interface.web_server_Maureen.id
   associate_with_private_ip = "10.0.1.50"
-  depends_on = [  aws_internet_gateway.Maureen_prod_Igw ]              
+  depends_on                = [aws_internet_gateway.Maureen_prod_Igw]
 }
 
 #Create unbuntu server and install/enable apache2
 resource "aws_instance" "maureen-web-server-instance" {
-ami             = "ami-053b0d53c279acc90"
-instance_type   = "t2.micro"
- availability_zone = "us-east-1a"
- key_name = "main.key"  
+  ami               = "ami-053b0d53c279acc90"
+  instance_type     = "t2.micro"
+  availability_zone = "us-east-1a"
+  key_name          = "main.key"
 
- network_interface {
-   device_index = 0
-   network_interface_id = aws_network_interface.web_server_Maureen.id
- }
+  network_interface {
+    device_index         = 0
+    network_interface_id = aws_network_interface.web_server_Maureen.id
+  }
 
- user_data = <<-EOF
+  user_data = <<-EOF
         #!/bin/bash
         sudo apt-get update
         sudo apt install apache2 -y
         sudo systemctl start apache2
         sudo bash  -c "echo your very first web server" > "/var/www/html/index.html"
         EOF
-        
-tags = {
+
+  tags = {
     Name = "Maureen-web_server_01"
-    }
- }
+  }
+}
 
 
 # resource "<provider>_<resource_type>" "name" {
